fdb986b5e027c3df2cfef70e0821ce28a43ff488
Adam Knight <aknightcoder@outlook.com>
2018-05-15 21:48:43 -0400

    Initial cut of LAC.

---
+++ Trees/LowestCommonAncestor.cs
@@ -0,0 +1,29 @@
+﻿namespace problemsolving.Trees
+{
+    public class LowestCommonAncestor
+    {
+        public int Find(TreeNode<int> root, int left, int right)
+        {
+            var ancestor = FindAncestor(root, left, right);
+
+            return ancestor.Value;
+        }
+
+        private TreeNode<int> FindAncestor(TreeNode<int> node, int left, int right)
+        {
+            if (node == null) return null;
+
+            if (node.Value == left || node.Value == right)
+                return node;
+
+            var leftCommonAncestor = FindAncestor(node.Left, left, right);
+
+            var rightCommonAncestor = FindAncestor(node.Right, left, right);
+
+            if (leftCommonAncestor != null && rightCommonAncestor != null)
+                return node;
+
+            return leftCommonAncestor ?? rightCommonAncestor;
+        }
+    }
+}
